069f55d42cddfe889cf750e2e9e5fd71e553a96d
Faizan Asad <p176056@nu.edu.pk>
2024-10-03 02:43:38 +0000

    yo

--- next.config.mjs
+++ next.config.mjs
@@ -7,6 +7,11 @@ const nextConfig = {
     // !! WARN !!
     ignoreBuildErrors: true,
   },
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
     webpack: (config) => {
         // Add rule for SVG files
         config.module.rules.push({
